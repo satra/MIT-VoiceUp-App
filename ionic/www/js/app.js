d20edeb11388a8ae40e4b5056331b44b8ee87192
suresh_TE <suresht@boston-technology.com>
2016-04-22 19:03:17 +0530

    Initial project push

---
+++ ionic/www/js/app.js
@@ -0,0 +1,99 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers','starter.services','surveyController','myProfileUpdate','homeController','customDirectives','ionicResearchKit', 'checklist-model', 'angular-dialgauge', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+// setup an abstract state for the tabs directive
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeCtrl'
+   // controller: 'SurveyCtrl'
+  })
+  
+  // setup an abstract state for the tabs directive
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+  .state('tab.Activities', {
+    url: '/Activities',
+    views: {
+      'tab-Activities': {
+        templateUrl: 'templates/Activities.html',
+        controller: 'SurveyCtrl'
+      }
+    }
+  })
+  
+  // Each tab has its own nav history stack:
+  .state('tab.dashboard', {
+    url: '/dashboard',
+    views: {
+      'tab-dashboard': {
+        templateUrl: 'templates/tab-activetasks.html',
+        controller: 'ActiveTasksCtrl'
+      }
+    }
+  })
+
+  // Each tab has its own nav history stack:
+  .state('tab.Profile', {
+    url: '/profile',
+    views: {
+      'tab-profile': {
+        templateUrl: 'templates/tab-profile-update.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+  .state('tab.learn', {
+    url: '/learn',
+    views: {
+      'tab-learn': {
+        templateUrl: 'templates/modal-online-resource.html',
+        controller: 'ActiveTasksCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/Activities');
+ // $urlRouterProvider.otherwise('/home');
+
+})
+
+.config(function($ionicConfigProvider) {
+  $ionicConfigProvider.tabs.style('standard');
+  $ionicConfigProvider.tabs.position('bottom');
+});
---
+++ ionic/www/js/controllers.js
@@ -0,0 +1,70 @@
+angular.module('starter.controllers', [])
+
+//=========Configuration to bring up the loader on hive when there is a web service call
+.config(function($httpProvider) {
+  $httpProvider.interceptors.push(function($rootScope) {
+    return {
+      request: function(config) {
+        $rootScope.$broadcast('loading:show')
+        return config
+      },
+      response: function(response) {
+        $rootScope.$broadcast('loading:hide')
+        return response
+      }
+    }
+  })
+})
+
+//======Show loader and hide on $scope Done 
+.run(function($rootScope, $ionicLoading) {
+  $rootScope.$on('loading:show', function() {
+    $ionicLoading.show({template: 'Loading data..'})
+  })
+  $rootScope.$on('loading:hide', function() {
+    $ionicLoading.hide()
+  })
+})
+
+
+// ==== Dummy contorller need to be removed later before production  ========
+.controller('ActiveTasksCtrl', function($scope, $ionicModal) {
+  $scope.openModalFingerTapActiveTask = function() {
+    $ionicModal.fromTemplateUrl('templates/modal-activetasks-fingertap.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  };
+
+  $scope.openModalVoiceActiveTask = function() {
+    $ionicModal.fromTemplateUrl('templates/modal-activetasks-voice.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.remove();
+  };
+
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+})
